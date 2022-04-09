2de95a44f08da590ee276391a2a6736ed921691f
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-04-08 21:39:22 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿{
+int numero;
+string paridade;
+
+Console.Write("Digete um Numero");
+numero = Convert.ToInt32(Console.ReadLine());
+
+paridade = (numero % 2) == 0 ? "par" : "imapar";
+
+Console.WriteLine($"{numero} é {paridade}");
+}
